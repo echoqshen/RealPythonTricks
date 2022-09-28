1b6381168f3751996c9cd77430725337893efaab
Echo Shen <echoqshen@gmail.com>
2022-09-28 09:23:19 -0400

    new tricks

---
+++ most_common_in_iterable.py
@@ -0,0 +1,4 @@
+import collections
+c=collections.Counter('Ihavegotbittenbymozzie!')
+c.most_common(4)
+print(c.most_common(4))
---
+++ permutation.py
@@ -0,0 +1,3 @@
+import itertools
+for p in itertools.permutations('ABCDE'):
+    print (p)
